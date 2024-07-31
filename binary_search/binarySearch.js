7cd4b0269243952598503c8739a5bc6e2db68baf
richardHaggioGwati <haggiog0844@gmail.com>
2024-07-31 12:39:32 +0200

    binarySearch

--- binary_search/binarySearch.js
+++ binary_search/binarySearch.js
@@ -1,18 +1,59 @@
-let binarySearchRecursiveFunction = function (arr, x, start, end) {
+const binarySearchRecursiveFunction = function (array, target, start, end) {
+    //Base condition
+    if (start > end ) return false
+
+    let middleIndex = Math.floor((start + end ) / 2)
+
+    if(array[middleIndex] === target) {
+        console.log("index: " , middleIndex)
+        return true
+    }
+
+    //if element at middle is smaller than target
+    //search left half of the middle index
+    if(array[middleIndex] > target) {
+        console.log("index: " , middleIndex)
+        return binarySearchRecursiveFunction(array, target, start, middleIndex - 1)
+    }
+    else {
+        console.log("index: " , middleIndex)
+        return binarySearchRecursiveFunction(array, target, middleIndex + 1, end)
+    }
 
 }
 
-// Test Cases
-let arr = [1, 5, 7, 10, 12, 14, 15, 18, 20, 22, 25, 27, 30, 64, 110, 220, 10, 50, 70, 100, 120, 140, 150, 180,
-    200, 220, 250, 270, 300, 640, 110, 220];
+const binarySearchIterativeFunction = (array, target) => {
 
-if (binarySearchRecursiveFunction(arr, 5, 0, arr.length - 1)) {
+    let end = array.length - 1
+    let start = 0
+
+    while (start <= end) {
+
+        let middleIndex = Math.floor((start + end ) / 2)
+
+        if (array[middleIndex] === target) {
+            console.log("index: " , middleIndex)
+            return true
+        } else if (array[middleIndex] < target) {
+            console.log("index: " , middleIndex)
+            start = middleIndex + 1
+        } else {
+            console.log("index: " , middleIndex)
+            end = middleIndex - 1
+        }
+    }
+}
+
+// Test Cases
+let testArray = [1, 5, 7, 10, 12, 14, 15, 18, 20, 22, 25, 27, 30, 64];
+
+if (binarySearchRecursiveFunction(testArray, 5, 0, testArray.length - 1)) {
     console.log("Element found!",);
 } else {
     console.log("Element not found!");
 }
 
-if (binarySearchRecursiveFunction(arr, 5, 0, arr.length - 1)) {
+if (binarySearchIterativeFunction(testArray, 5)) {
     console.log("Element found!");
 } else {
     console.log("Element not found!");
